f211087f63bb700cd5159f56aeedf38741ec9b6a
Chlorophyte <owenkbennett11@gmail.com>
2023-10-05 09:26:15 -0700

    Create isOnMap.m

---
+++ crabs/isOnMap.m
@@ -0,0 +1,7 @@
+function inBounds = isOnMap (x, y, width, height, buffer)
+  if (x > buffer && x < width-buffer && y > buffer && y < height-buffer)
+    inBounds = 1;
+  else
+    inBounds = 0;
+  endif
+endfunction
