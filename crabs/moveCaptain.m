c472adf4bb24d0c9dd35667f607294617b97a539
Chlorophyte <owenkbennett11@gmail.com>
2023-10-05 09:21:06 -0700

    Create moveCaptain.m

---
+++ crabs/moveCaptain.m
@@ -0,0 +1,43 @@
+function [xCapt,yCapt,thetaCapt] = moveCaptTest (cmd,x,y,theta,width,height, size)
+
+  xCapt=x;
+  yCapt=y;
+  thetaCapt=theta;
+
+  dTheta = pi/6;
+  dStep = 50;
+
+  if( cmd == "w")
+     %move forward
+     xCapt = x + dStep*cos(theta)
+     yCapt = y + dStep*sin(theta)
+
+     if(isOnMap(xTemp, yTemp,width,height,size)
+       xCapt = xTemp;
+       yCapt = yTemp;
+     else
+       xCapt = x;
+       yCapt = y;
+
+     thetaCapt = theta;
+
+     elseif (cmd == "a")
+     %rotate left
+     xCapt = x;
+     yCapt = y;
+     thetaCapt = theta - dTheta;
+
+     elseif(cmd == "d")
+     %turn right
+     xCapt = x;
+     yCapt = y;
+     thetaCapt = theta + dTheta;
+
+   else
+     %default
+       xCapt=x;
+       yCapt=y;
+       thetaCapt=theta;
+  endif
+
+endfunction
